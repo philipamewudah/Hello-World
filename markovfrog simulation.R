55b9ac048e05b35e20cc4c2a9c39d101d22a84f0
philipamewudah <52996636+philipamewudah@users.noreply.github.com>
2021-09-15 22:59:18 -0500

    Update and rename markovfrog.R to markovfrog simulation.R

--- markovfrog simulation.R
+++ markovfrog simulation.R
@@ -1,6 +1,8 @@
 x<-double(50)
 u_1<-runif(1,0,1)
 
+## The markov process at time 0
+
 if (0<=u_1 && u_1<=0.5){
   x[1]<-1
   } else if (0.5<u_1 && u_1<=3/4){
@@ -8,6 +10,10 @@ if (0<=u_1 && u_1<=0.5){
   } else {
   x[1]<-3
 }
+
+## The markov process after time 0
+
+
 for (i in 2:50) {
   u_i<-runif(1,0,1)
   if (x[i-1]==1){
